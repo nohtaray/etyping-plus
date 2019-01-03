4e546fb01415fb23de70cf9d57af6ee1821cc755
Yada <ydt.hran2@gmail.com>
2019-01-03 17:27:23 +0900

    リザルト拡張まわりの処理を切り出す

--- src/app.js
+++ src/app.js
@@ -63,12 +63,121 @@ const initializeExpandedResult = () => {
   });
 };
 
-// タイピング画面に注入される
-jQuery(function($) {
-  console.log('٩( ๑╹ ꇴ╹)۶');
+const expandResult = () => {
+  // 新しいウィンドウに #app をコピーして CSS 読み込み
+  // 'about:blank' を指定しないとリサイズができない https://stackoverflow.com/questions/35341839/chrome-zoom-is-not-working-in-child-tab
+  const newDoc = window.open('about:blank').document;
+  newDoc.write($('#app').html());
+  Array.from($('head>link[rel="stylesheet"]'), style => newDoc.write(style.outerHTML));
+  // TODO: src/ の中から読む
+  newDoc.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>');
+  const balloonJsSrc = Array.from(document.scripts, s => s.src).filter(s => s.includes('jquery.balloon'))[0];
+  newDoc.write(`<script type="text/javascript" src="${balloonJsSrc}"></script>`);
+  newDoc.write($('<script>').text(`( ${initializeExpandedResult.toString()} )()`).get(0).outerHTML);
+};
 
-  const ADD_ROWS = 1;
-  const ADD_HEIGHT = 32 * ADD_ROWS;
+const ADD_ROWS = 1;
+const ADD_HEIGHT = 32 * ADD_ROWS;
+
+// ---- リザルト拡張まわり ----
+function showWordDetail(charTimes, $sentence, missTimes, time, latency, miss) {
+  if (charTimes == null || charTimes.length === 0) {
+    $sentence.fadeTo(0, 0.6);
+    return;
+  }
+
+  // 文字別タイム
+  const keys = $sentence.text().trim().split('');
+  const $keys = keys.map((k, j) => {
+    const $key = $('<span>').text(k);
+    if (missTimes[j] == null) return $key.fadeTo(0, 0.6);
+
+    const loss = Math.max(...missTimes[j], -Infinity);
+    if (loss >= 0) {
+      $key.addClass('miss');
+    }
+    if (charTimes[j] == null) return $key.fadeTo(0, 0.6);
+
+    const toolTip = loss >= 0
+        ? `${(charTimes[j] / 1000).toFixed(3)} (${(loss / 1000).toFixed(3)})`
+        : `${(charTimes[j] / 1000).toFixed(3)}`;
+    // FIXME: 共通化。全画面の方コピペで定義してるので変えるときは一緒に変えてください
+    return $key.attr('data-tooltip', toolTip).balloon({
+      classname: 'time-balloon',
+      contents: toolTip,
+      showDuration: 64,
+      minLifetime: 0,
+      tipSize: 4,
+      showAnimation(d, c) { this.fadeIn(d, c); },
+      css: {
+        backgroundColor: '#f7f7f7',
+        color: '#636363',
+        boxShadow: '0',
+        opacity: 1,
+      },
+    });
+  });
+  $sentence.html($keys);
+
+  // ワード別詳細
+  const wordLength = charTimes.length;
+  const kpm = wordLength / time * 60000;
+  const rkpm = (wordLength - 1) / (time - latency) * 60000;
+  $sentence.parent().append($('<div>').css({
+    'font-size': '12px',
+    position: 'relative',
+    top: '-2px',
+  }).text(`latency: ${(latency / 1000).toFixed(3)}, kpm: ${kpm.toFixed(0)}, rkpm: ${rkpm.toFixed(0)}, miss: ${miss.toFixed(0)}`));
+}
+
+function extendResult({ misses, times, latencies, charTimes, missTimes, previousResult, expandResult, rkpm, latency }) {
+  // ワード詳細
+  const $sentences = $('#exampleList li .sentence').css('cursor', 'default');
+  for (let i = 0; i < $sentences.size(); i++) {
+    showWordDetail(charTimes[i], $sentences.eq(i), missTimes[i], times[i], latencies[i], misses[i]);
+  }
+
+  // 苦手キー邪魔なので除去
+  $('#current .result_data ul li').last().remove();
+  $('#prev .result_data ul li').last().remove();
+
+  // 初速
+  $('#current .result_data ul').append(`<li id="latency"><div class="title">Latency</div><div class="data">${latency.toFixed(3)}</div></li>`);
+  $('#prev .result_data ul').append(`<li id="previous_latency"><div class="data">${previousResult.latency == null ? '-' : previousResult.latency.toFixed(3)}</div></li>`);
+
+  // RKPM
+  $('#current .result_data ul').append(`<li id="rkpm"><div class="title">RKPM</div><div class="data">${rkpm.toFixed(2)}</div></li>`);
+  $('#prev .result_data ul').append(`<li id="previous_rkpm"><div class="data">${previousResult.rkpm == null ? '-' : previousResult.rkpm.toFixed(2)}</div></li>`);
+
+  // 見た目調整
+  $('#result article').css('height', `+=${ADD_HEIGHT}px`);
+  $('#result #exampleList').css('height', `+=${ADD_HEIGHT}px`);
+  $('#result .result_data').css('height', `+=${ADD_HEIGHT}px`);
+  $('#result #current').css('height', `+=${ADD_HEIGHT}px`);
+  $('#result #prev').css('height', `+=${ADD_HEIGHT}px`);
+  // マウスで選択できるようにする
+  $('#result').css({ 'user-select': 'text' });
+  // 全画面表示ボタン
+  $('<i class="fas fa-external-link-alt">').addClass('expand_result').appendTo($('#current')).on('click', () => {
+    expandResult();
+  }).css({
+    position: 'absolute',
+    padding: '8px',
+    top: '3px',
+    right: '5px',
+    'font-size': '14px',
+    color: '#636363',
+  }).hover(
+      function() { $(this).css('cursor', 'pointer'); },
+      function() { $(this).css('cursor', 'default'); },
+  );
+}
+
+// ---- ↑リザルト拡張まわり ----
+
+// タイピング画面に注入される
+jQuery(($) => {
+  console.log('٩( ๑╹ ꇴ╹)۶');
 
   {
     // 打鍵直後の初速表示が有効なときはランキング送信を失敗させる
@@ -84,8 +193,14 @@ jQuery(function($) {
   const showLatencyBalloon = (latency, target1, target2) => {
     const color = latency < target1 ? '#dff0d8' : latency < target2 ? '#fcf8e3' : '#f2dede';
     $('#sentenceText .entered').next().showBalloon({
-      contents: (latency / 1000).toFixed(3), classname: 'balloon', position: 'bottom left', offsetY: -4, offsetX: 50, tipSize: 0, showDuration: 64,
-      showAnimation: function(d, c) { this.fadeIn(d, c); },
+      contents: (latency / 1000).toFixed(3),
+      classname: 'balloon',
+      position: 'bottom left',
+      offsetY: -4,
+      offsetX: 50,
+      tipSize: 0,
+      showDuration: 64,
+      showAnimation(d, c) { this.fadeIn(d, c); },
       css: {
         backgroundColor: color,
         color: '#636363',
@@ -105,19 +220,6 @@ jQuery(function($) {
         console.error(e);
       }
     };
-  };
-
-  const expandResult = () => {
-    // 新しいウィンドウに #app をコピーして CSS 読み込み
-    // 'about:blank' を指定しないとリサイズができない https://stackoverflow.com/questions/35341839/chrome-zoom-is-not-working-in-child-tab
-    const newDoc = window.open('about:blank').document;
-    newDoc.write($('#app').html());
-    Array.from($('head>link[rel="stylesheet"]'), style => newDoc.write(style.outerHTML));
-    // TODO: src/ の中から読む
-    newDoc.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>');
-    const balloonJsSrc = Array.from(document.scripts, s => s.src).filter(s => s.includes('jquery.balloon'))[0];
-    newDoc.write(`<script type="text/javascript" src="${balloonJsSrc}"></script>`);
-    newDoc.write($('<script>').text(`( ${initializeExpandedResult.toString()} )()`).get(0).outerHTML);
   };
 
   let shouldShowLatencyBalloon;
@@ -192,124 +294,36 @@ jQuery(function($) {
       misses[misses.length - 1] += 1;
     }
 
-    // TODO: 消す
-    console.log({ misses, times, latencies });
-    console.log({ charTimes, missTimes });
-
-    // ワード詳細
-    const $sentences = $('#exampleList li .sentence').css('cursor', 'default');
-    for (let i = 0; i < $sentences.size(); i++) {
-      if (charTimes[i] == null || charTimes[i].length === 0) {
-        $sentences.eq(i).fadeTo(0, 0.6);
-        continue;
-      }
-
-      // 文字別タイム
-      const keys = $sentences.eq(i).text().trim().split('');
-      const $keys = keys.map((k, j) => {
-        const $key = $('<span>').text(k);
-        if (missTimes[i][j] == null) return $key.fadeTo(0, 0.6);
-
-        const loss = Math.max(...missTimes[i][j], -Infinity);
-        if (loss >= 0) {
-          $key.addClass('miss');
-        }
-        if (charTimes[i][j] == null) return $key.fadeTo(0, 0.6);
-
-        const toolTip = loss >= 0
-            ? `${(charTimes[i][j] / 1000).toFixed(3)} (${(loss / 1000).toFixed(3)})`
-            : `${(charTimes[i][j] / 1000).toFixed(3)}`;
-        // FIXME: 共通化。全画面の方コピペで定義してるので変えるときは一緒に変えてください
-        return $key.attr('data-tooltip', toolTip).balloon({
-          classname: 'time-balloon',
-          contents: toolTip,
-          showDuration: 64,
-          minLifetime: 0,
-          tipSize: 4,
-          showAnimation(d, c) { this.fadeIn(d, c); },
-          css: {
-            backgroundColor: '#f7f7f7',
-            color: '#636363',
-            boxShadow: '0',
-            opacity: 1,
-          },
-        });
-      });
-      $sentences.eq(i).html($keys);
-
-      // ワード別詳細
-      const wordLength = charTimes[i].length;
-      const kpm = wordLength / times[i] * 60000;
-      const rkpm = (wordLength - 1) / (times[i] - latencies[i]) * 60000;
-      $('#exampleList li').eq(i).append($('<div>').css({
-        'font-size': '12px',
-        'position': 'relative',
-        'top': '-2px',
-      }).text(`latency: ${(latencies[i] / 1000).toFixed(3)}, kpm: ${kpm.toFixed(0)}, rkpm: ${rkpm.toFixed(0)}, miss: ${misses[i].toFixed(0)}`));
-    }
-
-    // 苦手キー邪魔なので除去
-    $('#current .result_data ul li').last().remove();
-    $('#prev .result_data ul li').last().remove();
-
-    // 初速
-    const latenciesSum = latencies.map(a => +a).reduce((a, b) => a + b, 0);
-    const latency = latenciesSum / latencies.length / 1000;
-    $('#current .result_data ul').append(`<li id="latency"><div class="title">Latency</div><div class="data">${latency.toFixed(3)}</div></li>`);
-    $('#prev .result_data ul').append(`<li id="previous_latency"><div class="data">${previousResult.latency == null ? '-' : previousResult.latency.toFixed(3)}</div></li>`);
-
-    const parseTime = timeStr => {
+    const parseTime = (timeStr) => {
       if (timeStr.includes('分')) {
         const [m, s] = timeStr.split('分');
         return m * 60000 + s.replace('秒', '.') * 1000;
       }
       return timeStr.replace('秒', '.') * 1000;
     };
+    const latenciesSum = latencies.map(a => +a).reduce((a, b) => a + b, 0);
     const time = parseTime($('#current .result_data ul .title:contains("入力時間")').next().text());
     const charCount = +$('#current .result_data ul .title:contains("入力文字数")').next().text();
+    const latency = latenciesSum / latencies.length / 1000;
     const rkpm = (charCount - latencies.length) / (time - latenciesSum) * 60000;
-    $('#current .result_data ul').append(`<li id="rkpm"><div class="title">RKPM</div><div class="data">${rkpm.toFixed(2)}</div></li>`);
-    $('#prev .result_data ul').append(`<li id="previous_rkpm"><div class="data">${previousResult.rkpm == null ? '-' : previousResult.rkpm.toFixed(2)}</div></li>`);
+    extendResult({
+      misses, times, latencies, charTimes, missTimes, previousResult, expandResult, latency, rkpm,
+    });
 
+    previousResult = { latency, rkpm };
     misses = [];
     times = [];
     charTimes = [];
     missTimes = [];
     latencies = [];
-    previousResult = { latency, rkpm };
-
-    // 見た目調整
-    $('#result article').css('height', `+=${ADD_HEIGHT}px`);
-    $('#result #exampleList').css('height', `+=${ADD_HEIGHT}px`);
-    $('#result .result_data').css('height', `+=${ADD_HEIGHT}px`);
-    $('#result #current').css('height', `+=${ADD_HEIGHT}px`);
-    $('#result #prev').css('height', `+=${ADD_HEIGHT}px`);
-    // マウスで選択できるようにする
-    $('#result').css({ 'user-select': 'text' });
-    // 全画面表示ボタン
-    $('<i class="fas fa-external-link-alt">').addClass('expand_result').appendTo($('#current')).on('click', () => {
-      expandResult();
-    }).css({
-      'position': 'absolute',
-      'padding': '8px',
-      'top': '3px',
-      'right': '5px',
-      'font-size': '14px',
-      'color': '#636363',
-    }).hover(
-        function() { $(this).css('cursor', 'pointer'); },
-        function() { $(this).css('cursor', 'default'); },
-    );
   };
 
   const setResultShortcutKeysIfNotYet = () => {
     const eventNamespace = 'etypingbetterresult';
-    const isSet = ($._data(document).events.keydown || []).some(e => {
-      return e.namespace === eventNamespace;
-    });
+    const isSet = ($._data(document).events.keydown || []).some(e => e.namespace === eventNamespace);
     if (isSet) return;
 
-    $(document).on(`keydown.${eventNamespace}`, e => {
+    $(document).on(`keydown.${eventNamespace}`, (e) => {
       // F (Full result)
       if (e.keyCode === 70) {
         expandResult();
