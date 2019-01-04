33d55dda13fd12f8bec793bedcde0d62c1a4f585
Yada <ydt.hran2@gmail.com>
2019-01-05 01:38:58 +0900

    [WIP] Install chrome-extension-webpack-boilerplate

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,5 @@
+import '../img/e128.png';
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // popup で設定した localStorage は Content Script から参照できない
   if (Array.isArray(request) && request[0] === 'localStorage') {
