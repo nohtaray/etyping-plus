5828706d775bab210411cbba0a83ca9c3d064fd4
Yada <ydt.hran2@gmail.com>
2019-01-05 04:42:39 +0900

    Reformat webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,132 +1,131 @@
-var webpack = require("webpack"),
-    path = require("path"),
-    fileSystem = require("fs"),
-    env = require("./utils/env"),
-    CleanWebpackPlugin = require("clean-webpack-plugin"),
-    CopyWebpackPlugin = require("copy-webpack-plugin"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const fileSystem = require('fs');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const env = require('./utils/env');
 
 // load the secrets
-var alias = {};
-
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
-
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "otf"];
+const alias = {};
+const secretsPath = path.join(__dirname, (`secrets.${env.NODE_ENV}.js`));
+const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'otf'];
 
 if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
+  alias.secrets = secretsPath;
 }
 
-var options = {
+const options = {
   mode: env.NODE_ENV,
   entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js"),
-    content: path.join(__dirname, "src", "js", "content.js"),
-    app: path.join(__dirname, "src", "js", "app/main.js"),
+    popup: path.join(__dirname, 'src', 'js', 'popup.js'),
+    options: path.join(__dirname, 'src', 'js', 'options.js'),
+    background: path.join(__dirname, 'src', 'js', 'background.js'),
+    content: path.join(__dirname, 'src', 'js', 'content.js'),
+    app: path.join(__dirname, 'src', 'js', 'app/main.js'),
   },
   chromeExtensionBoilerplate: {
-    notHotReload: ["content", "app"]
+    notHotReload: ['content', 'app'],
   },
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader?sourceMap",
+        loader: 'style-loader!css-loader?sourceMap',
       },
       {
-        test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
-        loader: "file-loader?name=[name].[ext]",
-        exclude: /node_modules/
+        test: new RegExp(`\.(${fileExtensions.join('|')})$`),
+        loader: 'file-loader?name=[name].[ext]',
+        exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        loader: "html-loader",
-        exclude: /node_modules/
+        loader: 'html-loader',
+        exclude: /node_modules/,
       },
       // Fonts
       {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
-          mimetype: 'application/font-woff'
-        }
+          mimetype: 'application/font-woff',
+        },
       },
       {
         test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
-          mimetype: 'application/font-woff'
-        }
+          mimetype: 'application/font-woff',
+        },
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader"
+        loader: 'file-loader',
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
-          mimetype: 'application/octet-stream'
-        }
+          mimetype: 'application/octet-stream',
+        },
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
-          mimetype: 'image/svg+xml'
-        }
-      }
-    ]
+          mimetype: 'image/svg+xml',
+        },
+      },
+    ],
   },
   resolve: {
-    alias: alias
+    alias,
   },
   plugins: [
     // clean the build folder
-    new CleanWebpackPlugin(["build"]),
+    new CleanWebpackPlugin(['build']),
     // expose and write the allowed env vars on the compiled bundle
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
     }),
-    new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
-          description: process.env.npm_package_description,
-          version: process.env.npm_package_version,
-          ...JSON.parse(content.toString())
-        }))
-      }
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/manifest.json',
+        transform(content, path) {
+          // generates the manifest file using the package.json informations
+          return Buffer.from(JSON.stringify({
+            description: process.env.npm_package_description,
+            version: process.env.npm_package_version,
+            ...JSON.parse(content.toString()),
+          }));
+        },
+      }]),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
+      template: path.join(__dirname, 'src', 'options.html'),
+      filename: 'options.html',
+      chunks: ['options'],
     }),
-    new WriteFilePlugin()
-  ]
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'background.html'),
+      filename: 'background.html',
+      chunks: ['background'],
+    }),
+    new WriteFilePlugin(),
+  ],
 };
 
-if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
+if (env.NODE_ENV === 'development') {
+  options.devtool = 'cheap-module-eval-source-map';
 }
 
 module.exports = options;
